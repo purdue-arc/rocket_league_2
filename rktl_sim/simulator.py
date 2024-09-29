83abce9cca0cc2b46e4631f4bba0340d88f6d67a
sixfootsix50 <61257605+sixfootsix50@users.noreply.github.com>
2024-09-29 16:16:25 -0400

    Update simulator.py
    Added reset function and check for goals

--- rktl_sim/simulator.py
+++ rktl_sim/simulator.py
@@ -28,6 +28,7 @@ BALL_RADIUS = 6.85 / 2
 BALL_POS = (FIELD_WIDTH + GOAL_DEPTH) / 2, FIELD_HEIGHT / 2 # Starting position of the ball (x, y)
 BALL_ELASTICITY = 1
 BALL_FRICTION = 0.5
+BALL_DECELERATION = 0.1
 BALL_COLOR = pygame.Color("blue")
 
 #Sim Limits
@@ -38,9 +39,11 @@ class Car:
         self.body = pymunk.Body(CAR_MASS, pymunk.moment_for_box(CAR_MASS, CAR_SIZE))
         self.body.position = (x, y)
         self.body.angle = radians(angle)
+
         self.shape = pymunk.Poly.create_box(self.body, CAR_SIZE)
         self.shape.color = CAR_COLOR
         self.shape.friction = CAR_FRICTION
+
         self.space = space
         self.space.add(self.body, self.shape)
 
@@ -89,7 +92,30 @@ class Car:
         # Reset drift: Set velocity to only move in the direction of the car's facing angle
         self.body.velocity = self.forward_direction * self.reverse * min(self.body.velocity.length, MAX_SPEED)
         #Prints current velocity
-        print("\rVelocity:{:0.2f}".format(self.body.velocity.length),end="")
+        #print("\rVelocity:{:0.2f}".format(self.body.velocity.length),end="")
+
+class Ball:
+    def __init__(self, x, y, space, impulse= pymunk.Vec2d(0,0)):
+        self.inertia = pymunk.moment_for_circle(BALL_MASS, 0, BALL_RADIUS)
+        self.body = pymunk.Body(BALL_MASS, self.inertia)
+        self.body.position = x, y
+
+        self.shape = pymunk.Circle(self.body, BALL_RADIUS)
+        self.shape.friction = BALL_FRICTION
+        self.shape.elasticity = BALL_ELASTICITY
+        self.shape.color = BALL_COLOR
+
+        self.space = space
+        self.space.add(self.body, self.shape)
+
+        self.body.apply_impulse_at_local_point(impulse)
+    
+    def decelerate(self, dt):
+        self.body.velocity = (self.body.velocity.length - BALL_DECELERATION) * self.body.velocity.normalized()
+        #print("\rPos:{:0.2f}".format(self.body.position[0]),end="")
+    
+    def getPos(self):
+        return self.body.position
 
 class Game:
     def __init__(self):
@@ -97,14 +123,36 @@ class Game:
         self.screen = pygame.display.set_mode((FIELD_WIDTH + GOAL_DEPTH, FIELD_HEIGHT))
         self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
         self.clock = pygame.time.Clock()
+        self.leftscore = 0
+        self.rightscore = 0
         self.ticks = 60
         self.exit = False
         self.gameSpace = pymunk.Space()
+    
+    def checkGoal(self, ball, leftgoal, rightgoal, dt):
+        if ball.getPos()[0] < leftgoal:
+            self.rightscore += 1
+            self.reset()
+        elif ball.getPos()[0] > rightgoal:
+            self.leftscore += 1
+            self.reset()
+
+    def reset(self):
+        for c in self.cars:
+            self.gameSpace.remove(c.body, c.shape)
+        self.gameSpace.remove(self.ball.body, self.ball.shape)
+        self.addObjects()
+
+    def addObjects(self):
+        self.ball = Ball(BALL_POS[0], BALL_POS[1], self.gameSpace)
+        self.cars = [
+            Car((FIELD_WIDTH + GOAL_DEPTH) / 3, FIELD_HEIGHT / 2, self.gameSpace),
+            Car(2 * (FIELD_WIDTH + GOAL_DEPTH) / 3, FIELD_HEIGHT / 2+50, self.gameSpace, 180)
+        ]
 
     def run(self):
-        car1 = Car((FIELD_WIDTH + GOAL_DEPTH) / 3, FIELD_HEIGHT / 2, self.gameSpace)
-        car2 = Car(2 * (FIELD_WIDTH + GOAL_DEPTH) / 3, FIELD_HEIGHT / 2, self.gameSpace, 180)
-
+        self.addObjects()
+        
         # Walls in Field
         sideWallLength = (FIELD_HEIGHT - GOAL_HEIGHT) / 2
         static_lines = [
@@ -128,14 +176,15 @@ class Game:
             l.elasticity = FIELD_ELASTICITY
         self.gameSpace.add(*static_lines)
         
-        self.inertia = pymunk.moment_for_circle(BALL_MASS, 0, BALL_RADIUS, (0, 0))
-        self.ball = pymunk.Body(BALL_MASS, self.inertia)
-        self.ball.position = BALL_POS
-        self.ball_shape = pymunk.Circle(self.ball, BALL_RADIUS, (0, 0))
-        self.ball_shape.friction = BALL_FRICTION
-        self.ball_shape.elasticity = BALL_ELASTICITY
-        self.ball_shape.color = BALL_COLOR
-        self.gameSpace.add(self.ball, self.ball_shape)
+        
+        # self.inertia = pymunk.moment_for_circle(BALL_MASS, 0, BALL_RADIUS, (0, 0))
+        # self.ball = pymunk.Body(BALL_MASS, self.inertia)
+        # self.ball.position = BALL_POS
+        # self.ball_shape = pymunk.Circle(self.ball, BALL_RADIUS, (0, 0))
+        # self.ball_shape.friction = BALL_FRICTION
+        # self.ball_shape.elasticity = BALL_ELASTICITY
+        # self.ball_shape.color = BALL_COLOR
+        # self.gameSpace.add(self.ball, self.ball_shape)
 
         while not self.exit:
             self.dt = self.clock.get_time() / 1000
@@ -149,10 +198,13 @@ class Game:
             pressed = pygame.key.get_pressed()
 
             # Logic
-            car1.update(pressed, self.dt)
-            car2.update(pressed, self.dt)
+            for c in self.cars:
+                c.update(pressed, self.dt)
+            self.ball.decelerate(self.dt)
+            self.checkGoal(self.ball, GOAL_DEPTH, FIELD_WIDTH, self.dt)
 
             # Drawing
+            print("\rLeft: ", self.leftscore, " Right: ", self.rightscore,end="")
             pygame.display.set_caption("fps: " + str(self.clock.get_fps()))
             self.screen.fill(pygame.Color("white"))
             self.gameSpace.debug_draw(self.draw_options)
