837c6f09bd03716a568fd039bfbc716526983e59
DinoSage <anshag@gmail.com>
2024-10-20 16:25:33 -0400

    + mostly empty rktl_autonomy and rktl_interfaces packages

---
+++ src/rktl_autonomy/rktl_autonomy/ai_testing_node.py
@@ -0,0 +1,10 @@
+import rclpy
+from rclpy.node import Node
+
+class AiTesting(Node):
+    def __init__(self):
+        super().__init__('AI_Testing')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
