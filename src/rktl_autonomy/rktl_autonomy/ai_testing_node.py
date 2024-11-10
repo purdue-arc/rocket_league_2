0bffc447719453c99be653cc5d73880a7804d9d4
DinoSage <anshag@gmail.com>
2024-11-10 15:52:19 -0500

    [%] Incomplete, Disfunctional, Psuedocode Freamwork Written

--- src/rktl_autonomy/rktl_autonomy/ai_testing_node.py
+++ src/rktl_autonomy/rktl_autonomy/ai_testing_node.py
@@ -2,16 +2,17 @@ import rclpy
 from rclpy.node import Node
 from rktl_interfaces.msg import Field
 from rktl_interfaces.msg import CarAction
+from stable_baselines3 import PPO, A2C
 
 class AiTesting(Node):
     def __init__(self):
         super().__init__('AI_Testing')
         self.publisher = self.create_publisher(CarAction, 'car_output', 1)
         self.subscriber = self.create_subscription(Field, 'field_input', self.received_field_data, 1)
-        self.model = None
+        self.model = PPO.load(model_path, env=env)
 
     def received_field_data(self, field):
-        action = self.model.predict(field)
+        action, _state = self.model.predict(field)
         msg = CarAction()
         msg._id = 0
         msg._throttle = action[0]
@@ -24,7 +25,7 @@ def main(args=None):
     rclpy.init(args=args)
     node = AiTesting()
     rclpy.spin(node)
-    
+
     node.destroy_node()
     rclpy.shutdown()
 
--- src/rktl_autonomy/rktl_autonomy/ai_training_node.py
+++ src/rktl_autonomy/rktl_autonomy/ai_training_node.py
@@ -2,27 +2,33 @@ import rclpy
 from rclpy.node import Node
 from rktl_interfaces.msg import Field
 from rktl_interfaces.msg import CarAction
+from stable_baselines3 import PPO, A2C
+from rktl_autonomy.envs import TouchBall1
+
 
 class AiTraining(Node):
     def __init__(self):
         super().__init__('AI_Training')
         self.publisher = self.create_publisher(CarAction, 'car_output', 1)
         self.subscriber = self.create_subscription(Field, 'field_input', self.received_field_data, 1)
-        self.model = None
+        env = TouchBall1()
+        self.model = A2C("MultiInputPolicy", env, verbose=1, device="cuda")
+        self.model.learn(100000)
+
 
     def received_field_data(self, field):
-        action = self.model.predict(field)
-        msg = CarAction()
-        msg._id = 0
-        msg._throttle = action[0]
-        msg._steer = action[1]
+        self.field = field
 
-        self.publisher.publish(msg)
+    def callback_get_obs(self):
+        return self.field
+
+    def callback_take_action(self, action):
+        # code to publish car action to Car / simulator team
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = AiTesting()
+    node = AiTraining()
     rclpy.spin(node)
     
     node.destroy_node()
