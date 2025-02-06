957ec6b107b380097f3a2c833b94793286f42715
Witchtel01 <engineeringkerbals@gmail.com>
2025-02-06 01:28:46 -0500

    typo mb

--- rktl_simulator/rktl_simulator/testingSimPoint.py
+++ rktl_simulator/rktl_simulator/testingSimPoint.py
@@ -14,9 +14,9 @@ class tester(Node):
         
         def printFieldState(self, msg):
             self.recievedMessage = True
-            self.get_logger.info("recieved:")
-            self.get_logger.info(f"Ball Pose: x:{msg.ball_pose.x}, y:{msg.ball_pose.y}, id:{msg.ball_pose.id}")
-            self.get_logger.info(f"Car Pose: x:{msg.team1_poses[0].x}, y:{msg.team1_poses[0].y}, angle: {msg.team1_poses[0].angle_degrees}, id:{msg.team1_poses[0].id}")
+            self.get_logger().info("recieved:")
+            self.get_logger().info(f"Ball Pose: x:{msg.ball_pose.x}, y:{msg.ball_pose.y}, id:{msg.ball_pose.id}")
+            self.get_logger().info(f"Car Pose: x:{msg.team1_poses[0].x}, y:{msg.team1_poses[0].y}, angle: {msg.team1_poses[0].angle_degrees}, id:{msg.team1_poses[0].id}")
             self.broadcast()
             
         def broadcast(self):
