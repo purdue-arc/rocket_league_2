062ab756bdd42795674b81db543a193605a4e0c6
DinoSage <anshag@gmail.com>
2025-03-26 19:06:00 -0400

    [+] Evaluation Callback to dynamicall save model as it trains

--- rktl_autonomy/rktl_autonomy/train_agents.py
+++ rktl_autonomy/rktl_autonomy/train_agents.py
@@ -2,18 +2,25 @@ import gymnasium as gym
 import training_env
 
 from stable_baselines3 import PPO, A2C, DQN
+from stable_baselines3.common.callbacks import EvalCallback
 
-models_dir = r"~/models"
+models_dir = r"~/models/"
+logs_dir = r"~/logs/"
 TIMESTEPS = 500_000
+EVAL_FREQ = 10_000
 
 env = gym.make("training_env/TouchBall-v0", render_mode="rgb_array")
+eval_env = gym.make("training_env/TouchBall-v0")
+eval_callback = EvalCallback(eval_env, best_model_save_path=models_dir,
+                             log_path= logs_dir, eval_freq=EVAL_FREQ,
+                             render=False)
 
 model = DQN("MultiInputPolicy", env, verbose=1, device="cuda")
-model.learn(total_timesteps=TIMESTEPS)
+model.learn(total_timesteps=TIMESTEPS, callback=eval_callback)
 
 vec_env = model.get_env()
 obs = vec_env.reset()
-model.save(f"{models_dir}/{TIMESTEPS}")
+#model.save(f"{models_dir}/{TIMESTEPS}")
 for i in range(1000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
