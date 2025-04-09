e03f80e848724f73c296c6b0c0a4888cb31ba098
DinoSage <anshag@gmail.com>
2025-04-09 18:39:37 -0400

    [%] Rename to old train and test, remove entry points from setup.py

--- rktl_autonomy/setup.py
+++ rktl_autonomy/setup.py
@@ -20,8 +20,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'train_agents = rktl_autonomy.train_agents',
-            'test_agents = rktl_autonomy.test'
         ],
     },
 )
